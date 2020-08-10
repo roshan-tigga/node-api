4bd2928f5ba0ebdc9dfb21ef2733870c55b675f7
roshanphp <rtigga07@gmail.com>
2020-08-10 23:35:44 +0530

    ENV FILE UPDATE

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); 
-mongoose.connect('mongodb+srv://root:root@p01.ijesd.mongodb.net/fitdb?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true }, () =>{ 
+mongoose.connect('mongodb+srv://'+process.env.MONGO_USER+':'+process.env.MONGO_PASSWORD+'@p01.ijesd.mongodb.net/'+process.env.MONGO_DB+'?retryWrites=true&w=majority', { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true }, () =>{ 
 	console.log('DataBase is connect')
 });
 
